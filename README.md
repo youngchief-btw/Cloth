e25922f6d2df775ff48ac5b6a98afd612f12c09b
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2020-12-25 03:56:22 +0000

    Start work on Cloth's Minecraft: Java Edition API

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Cloth
 The multi-game, cross-platform, open-source modding API
+
+Game Status:
+- Rocket League | Planned
+- Minecraft: Java Edition | Currently being worked on
+- Minecraft: Bedrock Edition | Planned
---
+++ src/Minecraft/JavaEdition/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <fstream>
+
+// This file links all headers for Cloth's Minecraft: Java Edition together
+// In case you want to link against all of Cloth's Minecraft: Java Edition headers
+
